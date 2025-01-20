9d60e8e8f3b81aa18db3cd842d995f6341d97c3a
Rasmus Tønder <rtoender@hotmail.com>
2025-01-20 13:44:26 +0100

    testing

--- src/router/router.js
+++ src/router/router.js
@@ -8,14 +8,14 @@ import {html} from 'lit';
 export function routing(route) {
   if (route === '#map') {
     return html`
-    <map-viewer data-theme="light"></map-viewer>`
+    <map-viewer></map-viewer>`
   }
   if (route === '#bekendtgorelse') {
     return html`
-    <bekendtgorelse-page data-theme="light"></bekendtgorelse-page>`
+    <bekendtgorelse-page></bekendtgorelse-page>`
   }
   if (route === '#map-template') {
     return html`
-    <map-template data-theme="light"></map-template>`
+    <map-template></map-template>`
   }
 }
