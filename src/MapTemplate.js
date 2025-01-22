52ed7a99dbfc3d7fa2a8c73b4d42e1f9a8187f6e
Rasmus Tønder <rtoender@hotmail.com>
2025-01-22 09:50:23 +0100

    Update MapTemplate.js

--- src/MapTemplate.js
+++ src/MapTemplate.js
@@ -16,9 +16,6 @@ export class MapTemplate extends LitElement {
 
     return html`
           <map-viewer
-              .gmlFile="${gmlFile}"
-              .xmlFile="${xmlFile}"
-              .sldFile="${sldFile}">
           </map-viewer>
       `;
   }
